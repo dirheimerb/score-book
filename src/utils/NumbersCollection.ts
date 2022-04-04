db59ef080f8b2839e0c1fe49e3fc81e44fb4d092
Bradley Dirheimer <dirheimerb@gmail.com>
2022-04-03 22:08:56 -0400

    First commit
    Signed-off-by: Bradley Dirheimer <dirheimerb@gmail.com>

---
+++ src/utils/NumbersCollection.ts
@@ -0,0 +1,26 @@
+import { Sorter } from '../Controller/Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedListCollection } from './LinkedListCollection';
+
+
+
+export default function Collections() {
+const numbers = new NumbersCollection([10, 3, -5, 0]);
+const characters = new CharactersCollection('Bacon');
+const linkedList = new LinkedListCollection();
+
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+numbers.sort();
+numbers.print();
+
+characters.sort();
+characters.print();
+
+linkedList.sort();
+linkedList.print();
+}
